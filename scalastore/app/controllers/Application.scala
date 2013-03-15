59a5472d7c9567008ad021cdd9fc4f8e4ab03b49
Yann Simon <yann.simon.fr@gmail.com>
2013-03-15 16:58:54 +0100

    update to play 2.1.0

--- scalastore/app/controllers/Application.scala
+++ scalastore/app/controllers/Application.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
 import play.api.libs.json._
@@ -12,7 +11,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Application extends Controller {
   
   def index = Action {
-    Ok("Welcome to our website.")
+    Ok(views.html.index())
   }
 
   def search(query: String) = Action {
@@ -23,7 +22,7 @@ object Application extends Controller {
             val results = (response.json \ "results").as[Seq[JsObject]]
             Ok(results.map(_ \ "description").mkString("Found results: ", ", ", ".\n"))
           }
-          case _ => InternalServerError("Error calling search service.\n")
+          case _ => InternalServerError(s"Error calling search service.\nResponse status ${response.status}\n")
         }
       }
     }
@@ -37,7 +36,7 @@ object Application extends Controller {
       WS.url("http://localhost:9002/payments").post(Json.toJson(Map("amount" -> amount))).map { response =>
         response.status match {
           case 200 => Ok("Payment processed.\n")
-          case _ => InternalServerError("Error calling payment service.\n")
+          case _ => InternalServerError(s"Error calling payment service.\nResponse status ${response.status}\n")
         }
       }
     }
