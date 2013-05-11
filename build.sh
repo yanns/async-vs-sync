1d61b9cddb1cd1a6d04f17b0ab32807c2f6ebf13
Yann Simon <yann.simon.fr@gmail.com>
2013-05-11 17:51:46 +0200

    separate sync / async and deploy to tomcat

--- build.sh
+++ build.sh
@@ -4,33 +4,22 @@ DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "preparing applications"
 
-cd $DIRNAME/search
-sbt stage
+pushd $DIRNAME/sync/javastore
+mvn package
+popd
+rm -f $DIRNAME/server/apache-tomcat-7.0.40/webapps/*.war
+rm -rf $DIRNAME/server/apache-tomcat-7.0.40/webapps/*
+cp $DIRNAME/sync/javastore/target/javastore-0.1-SNAPSHOT.war $DIRNAME/server/apache-tomcat-7.0.40/webapps/ROOT.war
 
-cd $DIRNAME/payment
-sbt stage
+pushd $DIRNAME/backends/search
+sbt stage &
+popd
 
-cd $DIRNAME/scalastore
-sbt stage
+pushd $DIRNAME/backends/payment
+sbt stage &
+popd
 
+pushd $DIRNAME/async/scalastore
+sbt stage &
+popd
 
-echo "starting applications"
-
-$DIRNAME/search/target/start -Dhttp.port=9001 &
-$DIRNAME/payment/target/start -Dhttp.port=9002 &
-$DIRNAME/scalastore/target/start -Dhttp.port=9000 &
-
-
-sleep 2
-echo "all applications are running"
-read -p "press key to stop"
-
-echo "killing applications"
-search_pid=`cat $DIRNAME/search/RUNNING_PID`
-kill -SIGTERM $search_pid
-
-payment_pid=`cat $DIRNAME/payment/RUNNING_PID`
-kill -SIGTERM $payment_pid
-
-scalastore_pid=`cat $DIRNAME/scalastore/RUNNING_PID`
-kill -SIGTERM $scalastore_pid
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "starting applications"
+
+$DIRNAME/backends/search/target/start -Dhttp.port=9001 &
+$DIRNAME/backends/payment/target/start -Dhttp.port=9002 &
+$DIRNAME/async/scalastore/target/start -Dhttp.port=9000 &
+$DIRNAME/server/apache-tomcat-7.0.40/bin/catalina.sh start
+
+
+sleep 2
+echo "all applications are running"
+read -p "press key to stop"
+
+echo "killing applications"
+search_pid=`cat $DIRNAME/backends/search/RUNNING_PID`
+kill -SIGTERM $search_pid
+
+payment_pid=`cat $DIRNAME/backends/payment/RUNNING_PID`
+kill -SIGTERM $payment_pid
+
+scalastore_pid=`cat $DIRNAME/async/scalastore/RUNNING_PID`
+kill -SIGTERM $scalastore_pid
+
+$DIRNAME/server/apache-tomcat-7.0.40/bin/catalina.sh stop
