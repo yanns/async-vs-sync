fe456ccdc23707c83f0d0c69edfcdb0f2d5440f7
Yann Simon <yann.simon.fr@gmail.com>
2013-10-04 11:53:07 +0200

    handle invalid JSON

--- async/scalastore/app/models/StockQuery.scala
+++ async/scalastore/app/models/StockQuery.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.ws.WS
 import play.api.libs.json.JsObject
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Try}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -11,10 +11,9 @@ trait StockQuery {
   def searchStock(query: String): Future[Try[Seq[JsObject]]] = {
     WS.url("http://localhost:9001/search").withQueryString("query" -> query).get().map { response =>
       response.status match {
-        case 200 => {
-          val results = (response.json \ "results").as[Seq[JsObject]]
-          Success(results)
-        }
+        case 200 =>
+          Try((response.json \ "results").as[Seq[JsObject]])
+
         case _ => Failure(new Exception(s"Error calling search service.\nResponse status ${response.status}\n"))
       }
     }
